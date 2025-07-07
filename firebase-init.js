7e022d3ce0986db770986563d12a90ebcdea8615
KarolHalasi <karolhalasi12@gmail.com>
2025-07-07 20:39:55 +0200

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,4 +1,3 @@
-
 const firebaseConfig = {
   apiKey: "AIzaSyBxD-bUWAFmlAxzrgvdl5meMc9GjIDz0XA",
   authDomain: "akhtransapp.firebaseapp.com",
@@ -7,8 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "960895665450",
   appId: "1:960895665450:web:52d8efb64b76c2392602f5"
 };
-
-// Inicializácia (iba ak ešte nebola)
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
