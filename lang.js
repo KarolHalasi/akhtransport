68f8f13f55cc3be33f7009491d21145e7f85a9f7
KarolHalasi <karolhalasi12@gmail.com>
2025-07-07 17:15:42 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -7,7 +7,9 @@ const translations = {
     logout: "Odhlásiť sa",
 
     welcome: "Vitajte v AKH Transport",
-    intro: "Dvaja bratia, jedna dodávka a spoločná vášeň pre cestnú dopravu. Obaja milujeme šoférovanie, no život to zariadil tak, že jeden z nás viac sedí za volantom a druhý… za počítačom. Kým jeden brázdi cesty Slovenska, druhý má pod palcom logistiku a papierovačky. Spolu tvoríme zohraný tím, ktorý vám spoľahlivo doručí, čo treba – s dodávkou do 3,5 tony a úsmevom k tomu.",
+    intro: "Dvaja bratia, jedna dodávka a spoločná vášeň pre cestnú dopravu. Obaja milujeme šoférovanie, no život to zariadil tak, že jeden z nás viac sedí za volantom a druhý… za počítačom. Kým jeden brázdi cesty Slovenska, druhý má pod palcom logistiku a papierovačky. Spolu tvoríme zohraný tím, ktorý vám spoľahlivo doručí, čo treba – s dodávkou do 3,5 tony a úsmevom k tomu. Tešíme sa na spoluprácu! Karol a Attila
+
+",
 
     order_title: "Objednať prepravu",
     name: "Meno",
@@ -44,7 +46,7 @@ const translations = {
     logout: "Kijelentkezés",
 
     welcome: "Üdvözöljük az AKH Transportnál",
-    intro: "Két testvér, egy furgon és közös szenvedély a fuvarozás iránt. Mindketten imádunk vezetni, de az élet úgy hozta, hogy egyikünk inkább az utakat járja, míg a másik az irodában navigál. Míg az egyik testvér a volán mögött van, a másik az adminisztráció bajnoka. Együtt megbízható csapat vagyunk, 3,5 tonnáig bármit elszállítunk – mosollyal az arucnkon",
+    intro: "Két testvér, egy furgon és közös szenvedély a fuvarozás iránt. Mindketten imádunk vezetni, de az élet úgy hozta, hogy egyikünk inkább az utakat járja, míg a másik az irodában navigál. Míg az egyik testvér a volán mögött van, a másik az adminisztráció bajnoka. Együtt megbízható csapat vagyunk, 3,5 tonnáig bármit elszállítunk – mosollyal az arucnkon. Örömmel várjuk az együttműködést!Karol és Attila",
 
     order_title: "Szállítás rendelése",
     name: "Név",
@@ -81,7 +83,7 @@ const translations = {
     logout: "Log Out",
 
     welcome: "Welcome to AKH Transport",
-    intro: "Two brothers, one van, and a shared passion for transport. We both love driving, but life had other plans – one of us ended up on the road, the other behind the desk (someone has to deal with the paperwork, right?). While one brother conquers the highways, the other keeps the logistics running like clockwork. Together, we make a reliable team delivering across Slovakia with a 3.5-ton van – and a smile.",
+    intro: "Two brothers, one van, and a shared passion for transport. We both love driving, but life had other plans – one of us ended up on the road, the other behind the desk (someone has to deal with the paperwork, right?). While one brother conquers the highways, the other keeps the logistics running like clockwork. Together, we make a reliable team delivering across Slovakia with a 3.5-ton van – and a smile. Looking forward to working with you!Karol & Attila",
 
     order_title: "Order Transport",
     name: "Name",
